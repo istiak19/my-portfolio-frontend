07c38363fa8769afdb1e5c00aad867a0091e72ea
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-05 20:38:53 +0600

    create page updated

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Footer from "@/src/components/Footer/Footer";
 import Sidebar from "@/src/components/shared/Sidebar";
+import { SessionProvider } from "next-auth/react";
 
 export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <div className="flex flex-col min-h-dvh">
-            <main className="flex flex-1">
-                <Sidebar />
-                <div className="flex-1">{children}</div>
-            </main>
-            <Footer />
+            <SessionProvider>
+                <main className="flex flex-1">
+                    <Sidebar />
+                    <div className="flex-1">{children}</div>
+                </main>
+                <Footer />
+            </SessionProvider>
         </div>
     );
 }
--- src/app/(dashboard)/dashboard/project/page.tsx
+++ src/app/(dashboard)/dashboard/project/page.tsx
@@ -1,17 +1,17 @@
-import CreateProject from '@/src/components/CreateProject/CreateProject';
+import Project from '@/src/components/CreateProject/Project';
 
 export const metadata = {
-    title: "Create Project – Istiak Ahamed",
-    description:
-        "Portfolio of Istiak Ahamed, a passionate full-stack web developer specializing in building modern, responsive web applications using Next.js, React, and MongoDB.",
+  title: "Create Project – Istiak Ahamed",
+  description:
+    "Portfolio of Istiak Ahamed, a passionate full-stack web developer specializing in building modern, responsive web applications using Next.js, React, and MongoDB.",
 };
 
 const page = () => {
-    return (
-        <div>
-            <CreateProject />
-        </div>
-    );
+  return (
+    <div>
+      <Project />
+    </div>
+  );
 };
 
 export default page;
