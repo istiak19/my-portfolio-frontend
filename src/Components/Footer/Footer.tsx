415e08a56a0299ebd313dd7bbb9ebea738573333
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-18 21:39:56 +0600

    logo updated

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../../public/logo.png";
+import logo from "../../../public/logo-2.png";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className="container mx-auto flex flex-col items-center gap-4 text-center px-4">
                 {/* Logo and Name */}
                 <div className="flex flex-col items-center gap-2">
-                    <Image src={logo} alt="Logo" width={60} height={60} />
+                    <Image src={logo} alt="Logo" width={50} height={50} />
                     <span className="text-3xl font-semibold bg-gradient-to-r from-cyan-400 via-indigo-500 to-fuchsia-500 bg-clip-text text-transparent drop-shadow-md animate-text-gradient">
                         Istiak Ahamed
                     </span>
